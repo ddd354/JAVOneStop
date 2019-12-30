d87de4cd775b05662f4cf792aee4b39d1486c8a1
ddd <ddd@>
2019-12-29 23:07:32 -0500

    add basic file

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import os
 from JavHelper.app import create_app, create_app_backend
 
 if __name__ == '__main__':
-    #ui_instance = create_app()
-    #ui_instance.run()
+    ui_instance = create_app()
+    ui_instance.run()
 
-    os.environ['FLASK_ENV'] = 'development'
-    app = create_app_backend()
-    app.run()
+    #os.environ['FLASK_ENV'] = 'development'
+    #app = create_app_backend()
+    #app.run()
