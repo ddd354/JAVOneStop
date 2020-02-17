76e026ec72455734b5c366c2cd9b6e7cd252b4ff
ddd <ddd@>
2020-02-17 01:21:12 -0500

    fix typo

--- build_docker.sh
+++ build_docker.sh
@@ -4,7 +4,7 @@ containerName=jav_onestop
 
 # download user specific data from existing container
 docker cp $containerName:/usr/src/app/jav_manager.db .
-docker cp $containerName:/usr/src/app/setting.ini .
+docker cp $containerName:/usr/src/app/settings.ini .
 docker cp $containerName:/usr/src/app/115_cookies.json .
 
 docker build -t $imageName -f Dockerfile  .
@@ -13,4 +13,4 @@ echo Delete old container...
 docker rm -f $containerName
 
 echo Run new container...
-docker run -d -p 8009:8009 -v /mnt/???:/usr/data1 --name $containerName $imageName
+docker run -d -p 8009:8009 -v "${source_folder}":/usr/data1 --name $containerName $imageName
