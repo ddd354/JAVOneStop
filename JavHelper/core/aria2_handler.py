1d879834fde391466aade32c5667ff3cd1a9f067
DDD <ddd@ddd.com>
2020-03-15 02:45:31 -0400

    better 115 download error handle

--- JavHelper/core/aria2_handler.py
+++ JavHelper/core/aria2_handler.py
@@ -2,13 +2,14 @@ import aria2p
 
 from JavHelper.core.ini_file import return_default_config_string
 
-aria2 = aria2p.API(
-    aria2p.Client(
-        host=return_default_config_string('aria_address'),
-        port=int(return_default_config_string('aria_port') or 0),
-        secret=return_default_config_string('aria_token')
+def get_aria2():
+    return aria2p.API(
+        aria2p.Client(
+            host=return_default_config_string('aria_address'),
+            port=int(return_default_config_string('aria_port') or 0),
+            secret=return_default_config_string('aria_token')
+        )
     )
-)
 
 def verify_aria2_configs_exist():
     if not return_default_config_string('aria_address') or \
