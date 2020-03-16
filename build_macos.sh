f65fa7e37e5944d281582c46478b067a86566968
ddd <ddd@>
2020-03-16 11:36:42 -0400

    fix build step

--- build_macos.sh
+++ build_macos.sh
@@ -2,8 +2,16 @@
 
 pyinstaller --onedir \
     --icon "demo/icon.ico" \
+    --add-data="demo:demo" \
+    --add-data="translation.json:." \
+    --add-data="README.md:." \
+    --add-data="JAV_HELP.md:." \
     --add-data="JavHelper/templates:JavHelper/templates" \
     --add-data="JavHelper/static:JavHelper/static" \
+    --add-data="fillin/cloudscraper:cloudscraper" \
+    --hidden-import="js2py" \
+    --hidden-import="cloudscraper" \
+    --hidden-import="cloudscraper_exception" \
     --exclude-module="FixTk" \
     --exclude-module="tcl" \
     --exclude-module="tk" \
